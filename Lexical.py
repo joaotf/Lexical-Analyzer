eb401beb9adc1edfe633b057190799732cbb1317
João Tomé <joaotf159@gmail.com>
2019-09-11 12:58:20 -0300

    Add files via upload
    IDE with Lexical Analyzer.

---
+++ Lexical.py
@@ -0,0 +1,120 @@
+import re
+
+reservadas = [
+    "int",
+    "double",
+    "if",
+    "else",
+    "while",
+    "for",
+    "do",
+    "print",
+    "read"
+]
+
+agrupadores = [
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}"
+]
+
+comparadores = [
+    ">",
+    "<",
+    "==",
+    ">=",
+    "<=",
+]
+
+caractere = [
+    "!",
+    "@",
+    "$",
+    "%",
+    "¨¨",
+    "&",
+    "*",
+    "§"
+]
+
+class Lexical(object):
+    def __init__(self, source_code):
+        self.source_code = source_code
+
+
+    def token(self):
+        reservada = False
+        
+
+        tokens = []
+
+        source_code = self.source_code.split()
+
+        source_index = 0
+
+        while (source_index < len(source_code)):
+            word = source_code[source_index]
+
+            if(word in reservadas):
+                tokens.append([f"Token (Reservada) : {word}"])
+                reservada = True
+            else:
+                reservada = False
+            
+            if(re.match("\W+",word) == None):
+                if((re.match(r'[a-z]', word) or re.match(r'[A-Z]',word)) and reservada == False):
+                    tokens.append([f"Token (Identificador) : {word}"])
+            
+            if(word in caractere):
+                tokens.append([f"Token não identificado : {word}"])
+
+            elif(re.match("\d+\.\d+",word)):
+                tokens.append([f"Token (Double) : {word}"])
+
+            elif(re.match('\d+', word)):
+                tokens.append([f"Token (Inteiro) : {word}"])
+
+            elif(word in "=/*=-+"):
+                tokens.append([f"Token (Atribuição) : {word}"])
+            
+            elif(re.match("\(",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+                 
+            elif(re.match("\)",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+                 
+            elif(re.match("\[",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+                 
+            elif(re.match("\]",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+                 
+            elif(re.match("\>",word)):
+                tokens.append([f"Token (Comparador) : {word}"])
+                
+            elif(re.match("\<",word)):
+                tokens.append([f"Token (Comparador) : {word}"])
+                
+            elif(re.match("\=+",word)):
+                tokens.append([f"Token (Comparador) : {word}"])
+
+            elif(re.match("\{",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+            
+            elif(re.match("\}",word)):
+                tokens.append([f"Token (Agrupadores) : {word}"])
+
+            elif(re.match(r'\uFFFF',word)):
+                tokens.append([f"Token (Espaçador) : {word}"])
+            
+            elif("\#+" in word):
+                tokens.append([f"Token (Comentário) : {word}"])       
+
+
+            source_index += 1
+        
+        
+        return tokens
---
+++ Main.py
@@ -0,0 +1,14 @@
+import Lexical
+import IDE_Lexical as ide
+
+def main(args):
+    content = ""
+    with open(f"{args}",'r+') as file:
+        content = file.read()
+    
+    lex = Lexical.Lexical(content)
+    tokens = lex.token();
+
+    return tokens
+ide.doidao1()
+
